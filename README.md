bf870872ba175fd8355234adf149c44d365bf81f
SunhyeokChoe <chltnsgur2@daum.net>
2021-06-06 18:33:47 +0900

    [1.2.0] README 업데이트. 추가해야된다~

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
+### [My Notion](https://www.notion.so/8e3df61581404622a89e46b396de6d85)
+
 # springboot-aws-webservice
 스프링 부트, JPA, 스프링 시큐리티, AWS 인프라를 활용한 웹 애플리케이션 서비스
+
+# 개발환경 & 도구
+- Java 8 [JDK 1.8]
+- Gradle [4.8] ~ Gradle [4.10.2]
+-
+
+# Function Exception Code
+Code|Name|Meaning
+----|----|----
+
+...
---
+++ src/main/java/com/sunhyeokchoe/book/springboot/web/AliveController.java
@@ -0,0 +1,22 @@
+package com.sunhyeokchoe.book.springboot.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.
+RestController;
+
+/* << RestController >>
+ * 컨트롤러를 JSON을 반환하는 컨트롤러로 만들어 준다.
+ * 예전에는 @ResponseBody를 각 메서드마다 선언했던 것을 한번에 사용할 수 있도록 해준다고 보면 된다.
+ */
+@RestController
+public class AliveController {
+    /* GetMapping
+     * HTTP Method인 Get의 요청을 받을 수 있는 API를 만들어 준다.
+     * 예전에는 @RequestMapping으로 사용되었다.
+     * 이제 이 프로젝트는 /alive로 요청이 오면 문자열 "Im alive!"를 반환하는 기능을 가지게 된다.
+     */
+    @GetMapping("/alive")
+    public String hello() {
+        return "Im alive!";
+    }
+}
