06c9421af09f8e16be76d622c0406093ff47a9dc
SunhyeokChoe <chltnsgur2@daum.net>
2021-07-26 22:27:38 +0900

    [1.15.0] Add & Modify 게시글 업데이트 dto, 응답 dto 추가. 컨트롤러, 서비스, 도메인 수정.

--- src/main/java/com/sunhyeokchoe/book/springboot/services/posts/PostsService.java
+++ src/main/java/com/sunhyeokchoe/book/springboot/services/posts/PostsService.java
@@ -1,8 +1,10 @@
 package com.sunhyeokchoe.book.springboot.services.posts;
 
+import com.sunhyeokchoe.book.springboot.domain.posts.Posts;
 import com.sunhyeokchoe.book.springboot.domain.posts.PostsRepository;
+import com.sunhyeokchoe.book.springboot.web.dto.PostsResponseDto;
 import com.sunhyeokchoe.book.springboot.web.dto.PostsSaveRequestDto;
-
+import com.sunhyeokchoe.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,5 +18,23 @@ public class PostsService {
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity())
                 .getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    @Transactional
+    public PostsResponseDto findById (Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
     }
 }
