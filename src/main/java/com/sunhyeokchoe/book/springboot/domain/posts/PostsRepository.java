560b665855da1ef2686ff0c9bef193b55f256ec2
SunhyeokChoe <chltnsgur2@daum.net>
2021-08-04 23:52:16 +0900

    [1.21.0] Modify 인덱스 컨트롤러 내 게시글 수정 메서드 추가. 포스트 관련 API 컨트롤러 내 게시글 제거 메서드 추가.

--- src/main/java/com/sunhyeokchoe/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/sunhyeokchoe/book/springboot/domain/posts/PostsRepository.java
@@ -1,10 +1,14 @@
 package com.sunhyeokchoe.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 // Posts 엔티티 클래스로 데이터베이스에 접근 가능하게 해줄 인터페이스
 public interface PostsRepository extends JpaRepository<Posts, Long> { // [#1]
-
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
 
 /*
--- src/main/java/com/sunhyeokchoe/book/springboot/web/IndexController.java
+++ src/main/java/com/sunhyeokchoe/book/springboot/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.sunhyeokchoe.book.springboot.web;
 
 import com.sunhyeokchoe.book.springboot.services.posts.PostsService;
+import com.sunhyeokchoe.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -15,6 +17,14 @@ public class IndexController {
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
         return "index";
+    }
+
+    @GetMapping("/Posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 }
 
